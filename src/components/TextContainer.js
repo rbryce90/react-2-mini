6a824e0c239db6ca7b3546dc6dde26556b73f9e5
Richard Bond <richardbond@Richards-MacBook-Pro.local>
2019-01-21 13:58:43 -0700

    Dis Done

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -4,8 +4,9 @@ export default class TextContainer extends Component {
   constructor() {
     super()
     this.state = {
-      text: ''
-    }
+      text: '',
+    };
+    this.updateText = this.updateText.bind(this);
   }
 
   updateText(e) {
@@ -18,7 +19,11 @@ export default class TextContainer extends Component {
     return (
       <div className="textContainer">
         <textarea 
-          // style={  }
+          style={{ 
+            fontFamily:this.props.fontFamily,
+            fontSize:this.props.fontSize,
+            color:this.props.fontColor,
+           }}
           onChange={this.updateText}
           value={this.state.text} 
           placeholder='Start typing your thoughts here!'
